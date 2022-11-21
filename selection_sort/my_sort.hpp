b1d744e5f9306a8d4d47798298da982678d7ba48
camillemeuric <71633513+camillemeuric@users.noreply.github.com>
2022-11-21 18:08:51 +0100

    fini

---
+++ selection_sort/my_sort.hpp
@@ -0,0 +1,21 @@
+// Change me instead
+// All occurences of XXX need to be replaced with
+// something more meaningful
+
+
+template<typename iterator>
+
+void my_selection_sort( iterator begin, iterator end) {
+    for (iterator it = begin; it != end; ++it) {
+        iterator minIt = it;
+        for (iterator it2 = it; it2 != end; ++it2) {
+            if (*it2 < *minIt) {
+                minIt = it2;
+            }
+        }
+        // swap
+        auto tmp = *it;
+        *it = *minIt;
+        *minIt = tmp;
+    }
+}
